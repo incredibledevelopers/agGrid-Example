83d036c74b83aa179decf1e196d55ea0855df81d
Anvesh-Reddy <anveshreddymatta@gmail.com>
2018-08-21 10:29:21 +0530

    imported ag grid module

--- MyExample/src/app/app.module.ts
+++ MyExample/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular/main';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
